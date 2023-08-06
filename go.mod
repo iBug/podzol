650ed504f6103a15d43ac4e7964babdb6ae2fe3d
iBug <git@ibugone.com>
2023-08-06 17:17:20 +0800

    docker: Implement listing containers

--- go.mod
+++ go.mod
@@ -4,13 +4,14 @@ go 1.20
 
 require (
 	github.com/docker/docker v24.0.5+incompatible
+	github.com/docker/go-connections v0.4.0
 	github.com/spf13/cobra v1.7.0
+	golang.org/x/exp v0.0.0-20230801115018-d63ba01acd4b
 )
 
 require (
 	github.com/Microsoft/go-winio v0.6.1 // indirect
 	github.com/docker/distribution v2.8.2+incompatible // indirect
-	github.com/docker/go-connections v0.4.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
@@ -21,7 +22,7 @@ require (
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
-	golang.org/x/mod v0.8.0 // indirect
+	golang.org/x/mod v0.11.0 // indirect
 	golang.org/x/net v0.6.0 // indirect
 	golang.org/x/sys v0.5.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
--- pkg/docker/container.go
+++ pkg/docker/container.go
@@ -4,38 +4,97 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
+	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/go-connections/nat"
 	"github.com/ustclug/podzol/pkg"
+	"golang.org/x/exp/slices"
 )
 
 const Namespace = "podzol"
 
+// ContainerOptions is the options for Create, Remove and List.
 type ContainerOptions struct {
-	UserID        int
+	User          int
 	Token         string
 	ChallengeName string
 	Image         string
-	Port          int
+	Port          uint16
 	Lifetime      time.Duration
 }
 
-func (opts *ContainerOptions) ContainerName() string {
-	return fmt.Sprintf("%s_%d_%s_1", Namespace, opts.UserID, opts.ChallengeName)
+// ContainerLabel is the label data for containers.
+type ContainerLabel struct {
+	User      int           `json:"user"`
+	Challenge string        `json:"challenge"`
+	Lifetime  time.Duration `json:"lifetime"`
 }
 
+// Auxiliary struct for JSON.
+type containerLabelA ContainerLabel
+
+// Auxiliary struct for JSON.
+type containerLabelS struct {
+	containerLabelA
+
+	Lifetime string `json:"lifetime"`
+}
+
+// UnmarshalJSON implements json.Unmarshaler.
+func (c *ContainerLabel) UnmarshalJSON(b []byte) (err error) {
+	aux := &containerLabelS{containerLabelA: containerLabelA(*c)}
+	if err = json.Unmarshal(b, aux); err != nil {
+		return
+	}
+	c.Lifetime, err = time.ParseDuration(aux.Lifetime)
+	return
+}
+
+// ContainerInfo carrys the information of a container.
+type ContainerInfo struct {
+	Name     string    `json:"name"`
+	ID       string    `json:"id"`
+	Port     uint16    `json:"port"`
+	Deadline time.Time `json:"deadline"`
+}
+
+// Auxiliary struct for JSON.
+type containerInfoA ContainerInfo
+
+// Auxiliary struct for JSON.
+type containerInfoS struct {
+	containerInfoA
+
+	Deadline int64 `json:"deadline"`
+}
+
+// MarshalJSON implements json.Marshaler. Note that Deadline is exported as a Unix timestamp.
+func (c ContainerInfo) MarshalJSON() ([]byte, error) {
+	aux := &containerInfoS{containerInfoA: containerInfoA(c)}
+	aux.Deadline = c.Deadline.Unix()
+	return json.Marshal(aux)
+}
+
+// Construct container name from options.
+func (opts *ContainerOptions) ContainerName() string {
+	return fmt.Sprintf("%s_%d_%s_1", Namespace, opts.User, opts.ChallengeName)
+}
+
+// Construct JSON data from options.
 func (opts *ContainerOptions) Label() (string, error) {
-	b, err := json.Marshal(map[string]any{
-		"user":      opts.UserID,
-		"challenge": opts.ChallengeName,
-		"lifetime":  opts.Lifetime.String(),
+	b, err := json.Marshal(ContainerLabel{
+		User:      opts.User,
+		Challenge: opts.ChallengeName,
+		Lifetime:  opts.Lifetime,
 	})
 	return string(b), err
 }
 
+// Create a container from the given options.
 func (c *Client) Create(ctx context.Context, opts ContainerOptions) (string, error) {
 	label, err := opts.Label()
 	if err != nil {
@@ -65,13 +124,58 @@ func (c *Client) Create(ctx context.Context, opts ContainerOptions) (string, err
 		_ = c.Remove(ctx, opts)
 		return "", err
 	}
+
 	return resp.ID, err
 }
 
+// Remove a container.
 func (c *Client) Remove(ctx context.Context, opts ContainerOptions) error {
 	return c.c.ContainerRemove(ctx, opts.ContainerName(), types.ContainerRemoveOptions{
 		RemoveVolumes: true,
 		RemoveLinks:   true,
 		Force:         true,
 	})
+}
+
+// List containers.
+// Options are used to filter containers.
+// Only UserID, ChallengeName and Port are used.
+func (c *Client) List(ctx context.Context, opts ContainerOptions) ([]ContainerInfo, error) {
+	containers, err := c.c.ContainerList(ctx, types.ContainerListOptions{
+		All:     true,
+		Filters: filters.NewArgs(filters.Arg("label", pkg.ID)),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	var infos []ContainerInfo
+
+	for _, container := range containers {
+		labelStr := container.Labels[pkg.ID]
+		var label ContainerLabel
+		if err := json.Unmarshal([]byte(labelStr), &label); err != nil {
+			// Log error
+			continue
+		}
+
+		if opts.User != 0 && label.User != opts.User {
+			continue
+		}
+		if opts.ChallengeName != "" && label.Challenge != opts.ChallengeName {
+			continue
+		}
+		if opts.Port != 0 && !slices.ContainsFunc(container.Ports,
+			func(p types.Port) bool { return p.PublicPort == opts.Port }) {
+			continue
+		}
+
+		infos = append(infos, ContainerInfo{
+			Name:     strings.TrimPrefix(container.Names[0], "/"),
+			ID:       container.ID,
+			Port:     opts.Port,
+			Deadline: time.Unix(container.Created, 0).Add(label.Lifetime),
+		})
+	}
+	return infos, nil
 }
