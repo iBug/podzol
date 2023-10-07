8922ee873d93377a1b4e46e0d714a60c7a6a052a
iBug <git@ibugone.com>
2023-10-08 02:35:21 +0800

    Implement the body of TCP handler

--- pkg/server/http.go
+++ pkg/server/http.go
@@ -2,7 +2,11 @@ package server
 
 import (
 	"bufio"
+	"bytes"
+	"context"
+	"io"
 	"net"
+	"strings"
 	"sync"
 )
 
@@ -14,21 +18,22 @@ const BUFSIZE = 8192
 
 var pool = &sync.Pool{
 	New: func() any {
-		return make([]byte, 0, BUFSIZE)
+		buf := make([]byte, 0, BUFSIZE)
+		return &buf
 	},
 }
 
 func getBuffer() []byte {
-	return pool.Get().([]byte)
+	return *pool.Get().(*[]byte)
 }
 
 func putBuffer(buf []byte) {
-	pool.Put(buf[:0])
+	buf = buf[:0]
+	pool.Put(&buf)
 }
 
 func closeConn(conn *net.TCPConn) {
 	conn.Write([]byte("HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\nServer: ustclug/podzol\r\n\r\nMissing Host header\n"))
-	conn.Close()
 }
 
 // Create an HTTPServer from a Server.
@@ -37,9 +42,96 @@ func (s *Server) HTTPServer() *HTTPServer {
 }
 
 func (s *HTTPServer) Handle(conn *net.TCPConn) {
+	defer conn.Close()
+
 	// Note: This is still very simple and contains lots of allocations.
 	r := bufio.NewReaderSize(conn, BUFSIZE)
 	buf := getBuffer()
+	bufi := 0
 	defer putBuffer(buf)
-	defer r.Discard(0)
+	upstreamAddr := ""
+	for {
+		line, err := r.ReadSlice('\n')
+		if err != nil {
+			break
+		}
+		if bufi+len(line) > len(buf) {
+			// too large
+			closeConn(conn)
+			return
+		}
+
+		if bytes.EqualFold(line[:5], []byte("Host:")) {
+			hostname := string(bytes.TrimSpace(line[5:]))
+			hostname = strings.SplitN(hostname, ".", 2)[0]
+			upstreamAddr, err = s.s.docker.GetIP(context.TODO(), hostname)
+			if err != nil {
+				closeConn(conn)
+				return
+			}
+			break
+		}
+		if bytes.Equal(line, []byte("\r\n")) {
+			// all headers read
+			break
+		}
+
+		// accumulate to buf
+		bufi += copy(buf[bufi:], line)
+	}
+	if upstreamAddr == "" {
+		closeConn(conn)
+		return
+	}
+
+	// Connect to upstream
+	upstreamConnTemp, err := net.Dial("tcp", upstreamAddr)
+	if err != nil {
+		closeConn(conn)
+		return
+	}
+	upstreamConn := upstreamConnTemp.(*net.TCPConn)
+	defer upstreamConn.Close()
+	defer conn.Close()
+
+	chUp := make(chan int64)
+	chDown := make(chan int64)
+	go func() {
+		// copy from conn to upstream
+		n, err := io.Copy(upstreamConn, conn)
+		if err != nil {
+			// TODO: log error
+			_ = err
+		}
+		chUp <- n
+	}()
+	go func() {
+		// copy from upstream to conn
+		n, err := io.Copy(conn, upstreamConn)
+		if err != nil {
+			// TODO: log error
+			_ = err
+		}
+		chDown <- n
+	}()
+
+	// Collect statistics
+	uploadBytes := <-chUp
+	downloadBytes := <-chDown
+	// TODO: print or log them?
+	_, _ = uploadBytes, downloadBytes
+}
+
+func (s *HTTPServer) Listen() error {
+	l, err := net.Listen("tcp", s.s.httpAddr)
+	if err != nil {
+		return err
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			return err
+		}
+		go s.Handle(conn.(*net.TCPConn))
+	}
 }
